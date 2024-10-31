0c2af35cddae202d01a381da6504cd3b37fbc86d
huudungg <huudung2004kt@gmail.com>
2024-10-31 16:52:51 +0700

    validator schema

--- 02-nestjs-starter-master/src/users/users.service.ts
+++ 02-nestjs-starter-master/src/users/users.service.ts
@@ -3,7 +3,7 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User, UserDocument } from './schemas/user.schema';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Error, Model } from 'mongoose';
 import { genSaltSync, hashSync } from 'bcryptjs';
 
 @Injectable()

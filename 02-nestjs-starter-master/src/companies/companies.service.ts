8b6d2ef79ab8f1d4eefeaac874c09b53ae8b5608
huudungg <huudung2004kt@gmail.com>
2024-11-06 16:53:32 +0700

    filter

--- 02-nestjs-starter-master/src/companies/companies.service.ts
+++ 02-nestjs-starter-master/src/companies/companies.service.ts
@@ -18,7 +18,7 @@ export class CompaniesService {
   }
 
   async findAll(currentPage: number, limit: number, qs: string) {
-    const { filter, sort, projection, population } = aqp(qs);
+    const { filter, sort, population } = aqp(qs);
     delete filter.page;
     delete filter.limit;
 
@@ -28,12 +28,10 @@ export class CompaniesService {
     const totalItems = (await this.companyModel.find(filter)).length;
     const totalPages = Math.ceil(totalItems / defaultLimit);
 
-
     const result = await this.companyModel.find(filter)
       .skip(offset)
       .limit(defaultLimit)
-      // @ts-ignore: Unreachable code error
-      .sort(sort)
+      .sort(sort as any)
       .populate(population)
       .exec();
 

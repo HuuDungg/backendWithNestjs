689cfb00eb8c830cbcfa4490c8d0f575c67fb104
huudungg <huudung2004kt@gmail.com>
2024-11-06 11:00:53 +0700

    convert string to date expiration

--- 02-nestjs-starter-master/src/auth/auth.service.ts
+++ 02-nestjs-starter-master/src/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { User } from 'src/users/schemas/user.schema';
 import { UsersService } from 'src/users/users.service';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
+import ms from 'ms';
 @Injectable()
 export class AuthService {
     constructor(
@@ -22,7 +23,7 @@ export class AuthService {
         const payload = { username: user.name, sub: user._id, role: "HuuDung" };
         return {
             access_token: this.jwtService.sign(payload, {
-                secret: this.config.get<string>('JWT_SECRET_KEY'),
+                secret: String(ms(this.config.get<string>('JWT_SECRET_KEY'))),
                 expiresIn: this.config.get<string | number>('JWT_EXPIRATION_TIME')
             })
         };

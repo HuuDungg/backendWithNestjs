60c617911b4b72784f17f346aac2cf8c367764ef
huudungg <huudung2004kt@gmail.com>
2024-11-08 10:22:35 +0700

    fix class nested validator

--- 02-nestjs-starter-master/src/companies/dto/create-company.dto.ts
+++ 02-nestjs-starter-master/src/companies/dto/create-company.dto.ts
@@ -1,65 +1,16 @@
-import { Type } from 'class-transformer';
-import { IsEmail, IsNotEmpty, IsNotEmptyObject, IsObject, ValidateNested } from 'class-validator';
-import mongoose from 'mongoose';
-
-
-class Company {
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+export class CreateCompanyDto {
     @IsNotEmpty()
-    _id: mongoose.Schema.Types.ObjectId;
+    @IsString()
+    name: string;
 
     @IsNotEmpty()
-    name: string;
-}
-
-export class CreateUserDto {
-
-    @IsNotEmpty({ message: 'Name không được để trống', })
-    name: string;
-
-    @IsEmail({}, { message: 'Email không đúng định dạng', })
-    @IsNotEmpty({ message: 'Email không được để trống', })
-    email: string;
-
-    @IsNotEmpty({ message: 'Password không được để trống', })
-    password: string;
-
-    @IsNotEmpty({ message: 'Age không được để trống', })
-    age: number;
-
-    @IsNotEmpty({ message: 'Gender không được để trống', })
-    gender: string;
-
-    @IsNotEmpty({ message: 'Address không được để trống', })
+    @IsString()
     address: string;
 
-    @IsNotEmpty({ message: 'Role không được để trống', })
-    role: string;
+    @IsNotEmpty()
+    @IsString()
+    description: string;
 
-    @IsNotEmptyObject()
-    @IsObject()
-    @ValidateNested()
-    @Type(() => Company)
-    company: Company;
-}
 
-export class RegisterUserDto {
-
-    @IsNotEmpty({ message: 'Name không được để trống', })
-    name: string;
-
-    @IsEmail({}, { message: 'Email không đúng định dạng', })
-    @IsNotEmpty({ message: 'Email không được để trống', })
-    email: string;
-
-    @IsNotEmpty({ message: 'Password không được để trống', })
-    password: string;
-
-    @IsNotEmpty({ message: 'Age không được để trống', })
-    age: number;
-
-    @IsNotEmpty({ message: 'Gender không được để trống', })
-    gender: string;
-
-    @IsNotEmpty({ message: 'Address không được để trống', })
-    address: string;
 }

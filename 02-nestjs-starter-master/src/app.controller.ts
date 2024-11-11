fc54d55d35d000ee4d2a4f0b8e879c48f9900a63
huudungg <huudung2004kt@gmail.com>
2024-11-11 15:09:47 +0700

    logout api and remove cookie before logout

--- 02-nestjs-starter-master/src/app.controller.ts
+++ 02-nestjs-starter-master/src/app.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Post, UseGuards, Get, Res, Req } from '@nestjs/common';
 import { AuthService } from './auth/auth.service';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
-import { Public } from './decorator/customize';
+import { Public, ResponseMessage } from './decorator/customize';
 import { Request, Response } from 'express';
 import { ConfigService } from '@nestjs/config';
 import ms from 'ms';
@@ -20,18 +20,28 @@ export class AppController {
   ) {
     return this.authService.login(req.user, response);
   }
+  @ResponseMessage("Get profile")
   @Get('/profile')
   async profile(@User() user: IUser) {
     return user;
   }
 
   @Public()
+  @ResponseMessage("Get info with refresh token")
   @Get('/refresh')
-  async refresh(@Req() request: Request) {
-    const refresh = request.cookies["refresh_token"]
+  async refresh(@Req() response: Response) {
+    const refresh = response.clearCookie["refresh_token"]
     const result = await this.authService.processNewToken(refresh);
     return {
       result
     }
+  }
+
+  @Public()
+  @ResponseMessage("Logout successfully")
+  @Get('/logout')
+  async logout(@Req() request: Request) {
+    request.cookies["refresh_token"]
+    return "ok";
   }
 }
